479e91c258a2a471a83213a00bdba6f3d54ec82d
Joseph Kim <gyuhankim@gmail.com>
2018-06-12 11:38:41 -0400

    add authorization feature

---
+++ routes/users.js
@@ -0,0 +1,131 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+//Testing purposes only
+router.get('/', (req, res, next) => {
+
+  User.find()
+    .sort('name')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+
+});
+
+//Create new user
+// router.post('/', (req, res, next) => {
+//   const { fullname, username, password } = req.body;
+
+//   const newUser = { fullname, username, password };
+
+//   User.create(newUser)
+//     .then(result => {
+//       res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+//     })
+//     .catch(err => {
+//       if (err.code === 11000) {
+//         err = new Error('Username already exists');
+//         err.status = 400;
+//       }
+//       next(err);
+//     });
+// });
+
+router.post('/', (req, res, next) => {
+  const { fullname, username, password } = req.body;
+
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    const err = new Error(`Missing '${missingField}' in request body`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const stringFields = ['username', 'password', 'fullname'];
+  const nonStringField = stringFields.find(field =>
+    field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    const err = new Error('Incorrect field type: expected string');
+    err.status = 422;
+    return next(err);
+  }
+
+  const explicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicitlyTrimmedFields.find( field => 
+    req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    const err = new Error('Cannot start or end with whitespace');
+    err.status = 422;
+    return next(err);
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
+    .then (result => {
+      return res.status(201).location(`/api/users/${result.id}`).json(result);
+    })
+    .catch (err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+
+});
+
+module.exports = router;
